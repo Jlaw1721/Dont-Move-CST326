c9c08d67422d1ed8a4a254b63b42da0b907da0d5
DickermanJosh <123334042+DickermanJosh@users.noreply.github.com>
2023-04-20 15:44:50 -0700

    Added monster movement, both based on player movement and camera movement
    Made modifications to the PlayerMovement script to communicate with the monsterMovement script
    
    Created a MonsterMovement script that is in charge of setting the monster's navmesh agent's position to the player's position, and moving the monster based on how much the player is currently moving their mouse or the player model.
    
    I also set up a placeholder monster in the playerMovementTestScene for a visualization of what is happening.

--- Don't Move/Assets/Scripts/Camera Scripts/PlayerCamController.cs
+++ Don't Move/Assets/Scripts/Camera Scripts/PlayerCamController.cs
@@ -1,25 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamController : MonoBehaviour
 {
-    
-    
-    public Transform currentOrientation;
-    
-    public Vector2 mouseSensitivity;
+    [SerializeField] private Transform currentOrientation;
+    [SerializeField] private Vector2 mouseSensitivity;
     private Vector2 _mouseInput;
     private Vector2 _currentRotation;
-    
+    private float prevSpeed;
+
+    private static PlayerCamController _instance;
+    public static PlayerCamController Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+    }
+
     private void Start()
     {
         // Locking the cursor in the center of the screen and making it invisible to the player
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        
+        // Initialize previous rotation to current rotation at start
+        var transform1 = transform;
+        var rotation = transform1.rotation;
+        _currentRotation = new Vector2(rotation.x, rotation.y);
     }
-
-   
+    
     private void Update()
     {
         MoveCamera();
@@ -33,13 +42,22 @@ public class PlayerCamController : MonoBehaviour
 
         // Clamping rotation so the player can only look up or down 90 degrees
         _currentRotation.x = Mathf.Clamp(_currentRotation.x, -90f, 90f);
-        
+
         _currentRotation.x -= _mouseInput.y;
         _currentRotation.y += _mouseInput.x;
         
         // Rotating Camera across x and y axis
         transform.rotation = Quaternion.Euler(_currentRotation.x,_currentRotation.y, 0f);
-        // Rotating Camera across y axis
+        // Rotating orientation object on player across y axis
         currentOrientation.rotation = Quaternion.Euler(0f, _currentRotation.y, 0f);
+    }
+
+    public float GetCameraMovement()
+    {
+        // Getting the rotation amount of the current frame
+        var rotation = transform.rotation;
+        Vector2 currRotation = new Vector2(rotation.x, rotation.y);
+
+        return currRotation.magnitude;
     }
 }
